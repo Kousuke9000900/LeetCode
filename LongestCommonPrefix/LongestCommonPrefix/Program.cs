159ab0ca804a67c3e5916b24c9256ac80f1845cd
KS <Kousuke9000900@gmail.com>
2020-10-02 17:47:25 +0900

    LongestCommonPrefixの回答（未完）

---
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 配列内の要素で共通する接頭語を取り出す
+        /// </summary>
+        /// <param name="strs">対象の配列</param>
+        /// <returns>取り出した共通の接頭語</returns>
+        public string LongestCommonPrefix(string[] strs)
+        {
+
+            int prefixLength = 0;   // 接頭語の文字数
+
+            // 接頭語の文字数を算出→文字数が最も少ない値を取り出し、それの文字数を代入
+            
+
+        }
+    }
+}
