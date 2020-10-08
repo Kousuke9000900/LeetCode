e1ecc9d4ecc3f42f7666e20eeba570614c7251ce
Kousuke9000900 <kousuke9000900@gmail.com>
2020-10-08 21:45:54 +0900

    LongestCommonPrefix回答（未完）

--- LongestCommonPrefix/LongestCommonPrefix/Program.cs
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -19,11 +19,41 @@ namespace LongestCommonPrefix
         /// <returns>取り出した共通の接頭語</returns>
         public string LongestCommonPrefix(string[] strs)
         {
+            int leastWordCount = 0;     // 配列内で最も少ない文字数
+            string commonPrefix = "";   // 共通する接頭語
+            int Count = 0;              // 配列内の最も少ない文字列が格納されている番号
 
-            int prefixLength = 0;   // 接頭語の文字数
+            // 接頭語の文字数を算出
+            for(int i = 0; i < strs.Length; i++)
+            {
+                // 最小値を算出（最初のみ強制代入）
+                if (leastWordCount == 0 || leastWordCount > strs[i].Length)
+                {
+                    leastWordCount = strs[i].Length;   // 文字数
+                    Count = i;
+                }                
+            }
 
-            // 接頭語の文字数を算出→文字数が最も少ない値を取り出し、それの文字数を代入
-            
+            // 接頭語取り出し（最低限最小値分試行が必要）
+            for (int i = 0; i < leastWordCount; i++)
+            {
+                string firsrCharacter;
+                firsrCharacter = strs[Count].Substring(i, 1);
+
+                // 比較
+                for(int j = 0; j < strs.Length; j++)
+                {
+                    if (strs[j].IndexOf(firsrCharacter) != 0)
+                    {
+                        return commonPrefix;    // 終了時点で返す
+                    }
+
+                }
+
+                commonPrefix += firsrCharacter; // 共通する1文字
+            }
+
+            return commonPrefix;
 
         }
     }
