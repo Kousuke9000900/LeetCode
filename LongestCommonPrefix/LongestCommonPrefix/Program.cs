45d008c54a7b6f3f18f91c6ad07b3124095ad0fc
KS <Kousuke9000900@gmail.com>
2020-10-13 10:42:04 +0900

    LongestCommonPrefixの回答

--- LongestCommonPrefix/LongestCommonPrefix/Program.cs
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -10,6 +10,10 @@ namespace LongestCommonPrefix
     {
         static void Main(string[] args)
         {
+            // 試行用のプログラム
+            string[] testArgment = new string[2] {"aa","ab"};
+            Console.WriteLine("抽出した文字列は" + LongestCommonPrefix(testArgment) + "でした");
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -17,44 +21,53 @@ namespace LongestCommonPrefix
         /// </summary>
         /// <param name="strs">対象の配列</param>
         /// <returns>取り出した共通の接頭語</returns>
-        public string LongestCommonPrefix(string[] strs)
+        public static string LongestCommonPrefix(string[] strs)
         {
-            int leastWordCount = 0;     // 配列内で最も少ない文字数
-            string commonPrefix = "";   // 共通する接頭語
-            int Count = 0;              // 配列内の最も少ない文字列が格納されている番号
+            int leastWordCount = 0;         // 配列内で最も少ない文字数
+            string commonPrefix = "";       // 共通する接頭語          
+            int currentCharacterNumber = 0; // 比較する文字列目
+
+            // 空文字確認（空文字が含まれていれば共通の接頭語は存在しない）
+            foreach (string str in strs)
+            {
+                if (str == "")
+                {
+                    return commonPrefix;
+                }
+            }
 
             // 接頭語の文字数を算出
-            for(int i = 0; i < strs.Length; i++)
+            for (int i = 0; i < strs.Length; i++)
             {
                 // 最小値を算出（最初のみ強制代入）
                 if (leastWordCount == 0 || leastWordCount > strs[i].Length)
                 {
                     leastWordCount = strs[i].Length;   // 文字数
-                    Count = i;
                 }                
-            }
+            }            
 
             // 接頭語取り出し（最低限最小値分試行が必要）
             for (int i = 0; i < leastWordCount; i++)
             {
                 string firsrCharacter;
-                firsrCharacter = strs[Count].Substring(i, 1);
+                firsrCharacter = strs[0].Substring(i, 1);
 
                 // 比較
                 for(int j = 0; j < strs.Length; j++)
                 {
-                    if (strs[j].IndexOf(firsrCharacter) != 0)
+                    string compareStr;
+                    compareStr = strs[j].Substring(currentCharacterNumber, 1);  // 比較する文字列を取得
+
+                    if (compareStr.IndexOf(firsrCharacter) == -1)  // 該当しない場合は-1
                     {
                         return commonPrefix;    // 終了時点で返す
                     }
 
                 }
-
                 commonPrefix += firsrCharacter; // 共通する1文字
+                currentCharacterNumber++;
             }
-
             return commonPrefix;
-
         }
     }
 }
