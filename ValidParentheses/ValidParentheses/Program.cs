a1f92bb6c087a98eac763cc00e0098a5400edbb2
KS <Kousuke9000900@gmail.com>
2020-10-15 13:46:35 +0900

    ValidParenthesesの回答

--- ValidParentheses/ValidParentheses/Program.cs
+++ ValidParentheses/ValidParentheses/Program.cs
@@ -11,7 +11,7 @@ namespace ValidParentheses
         static void Main(string[] args)
         {
             // 試行用のプログラム
-            string testArgment = "(())";
+            string testArgment = "(){}}{";
             Program program = new Program();
             Console.WriteLine("試行結果は" + program.IsValid(testArgment) + "でした");
             Console.ReadKey();
@@ -23,6 +23,7 @@ namespace ValidParentheses
         /// <param name="s">括弧の組合せ</param>
         /// <returns>true:正しい括弧の組合せ false:不正な括弧の組合せ</returns>
         /// <remarks>例→():true ([)]→false: (]→false</remarks>
+        /// <remarks>leetcode内のテストケースを694msで通過（相当遅い）</remarks>
         public bool IsValid(string s)
         {
             // 強制的にfalseのパターン→1.最初が閉じ 2.1文字のみ 3.開きが存在しない 4.閉じが存在しない 5.開きと閉じの個数が一致しない
@@ -48,8 +49,8 @@ namespace ValidParentheses
                 return false;
             }
 
-            // 対応する括弧か確認（試行回数何回やるのか）
-                for (double i = 0; i <= (1.0 / 2.0 * System.Math.Pow(s.Length / 2.0 , 2)) + (3.0 / 2.0 * s.Length / 2); i++) // 階差数列で試行回数を求める
+            // 対応する括弧か確認（確認した括弧は削除）
+                for (int i = 0; i < subStringS.Length; i++) // 括弧が全て削除されるまで実行
             {
                 // 括弧抽出
                 currentParenthesis = subStringS.Substring(currentWordCount, 1);
@@ -58,63 +59,66 @@ namespace ValidParentheses
                 // 括弧が閉じか確認
                 switch (currentParenthesis)
                 {
-                    case ")":                        
-                        firstParenthesis = subStringS.Substring(currentWordCount - 1, 1);  // 最も内側の括弧開き
+                    case ")":
+                        if (currentWordCount == 0)  // 最初が閉じ→対応する開きは存在しない
+                        {
+                            return false;
+                        }
+
+                        // 最も内側の括弧開きを確認
+                        firstParenthesis = subStringS.Substring(currentWordCount - 1, 1);
                         if (firstParenthesis != "(")
                         {
                             return false;
                         }
                         else
                         {
-                            if (currentWordCount == 1)
-                            {
-                                subStringS = subStringS.Substring(0, 1) + subStringS.Substring(currentWordCount + 1, subStringS.Length - (currentWordCount + 1));　// 確認後の括弧を削除（左から抽出＋右から抽出を合わせる）
-                            }
-                            else
-                            {
-                                subStringS = subStringS.Substring(0, currentWordCount - 1) + subStringS.Substring(currentWordCount + 1, subStringS.Length - (currentWordCount + 1));　// 確認後の括弧を削除（左から抽出＋右から抽出を合わせる）
-                            }                            
-                            currentWordCount = 0;  // 初期化
+                            subStringS = subStringS.Remove(currentWordCount - 1, 2); // ()を削除
+                            // 初期化
+                            currentWordCount = 0;
+                            i = -1; // i = 0で再度処理する為
                         }
                         break;
 
                     case "]":
-                        firstParenthesis = subStringS.Substring(currentWordCount - 1, 1);  // 最も内側の括弧開き
+                        if (currentWordCount == 0)  // 最初が閉じ→対応する開きは存在しない
+                        {
+                            return false;
+                        }
+
+                        // 最も内側の括弧開きを確認
+                        firstParenthesis = subStringS.Substring(currentWordCount - 1, 1);
                         if (firstParenthesis != "[")
                         {
                             return false;
                         }
                         else
                         {
-                            if (currentWordCount == 1)
-                            {
-                                subStringS = subStringS.Substring(0, 1) + subStringS.Substring(currentWordCount + 1, subStringS.Length - (currentWordCount + 1));　// 確認後の括弧を削除（左から抽出＋右から抽出を合わせる）
-                            }
-                            else
-                            {
-                                subStringS = subStringS.Substring(0, currentWordCount - 1) + subStringS.Substring(currentWordCount + 1, subStringS.Length - (currentWordCount + 1));　// 確認後の括弧を削除（左から抽出＋右から抽出を合わせる）
-                            }
-                            currentWordCount = 0;  // 初期化
+                            subStringS = subStringS.Remove(currentWordCount - 1, 2); // []を削除
+                            // 初期化
+                            currentWordCount = 0;
+                            i = -1; // i = 0で再度処理する為
                         }
                         break;
 
                     case "}":
-                        firstParenthesis = subStringS.Substring(currentWordCount - 1, 1);  // 最も内側の括弧開き
+                        if (currentWordCount == 0)  // 最初が閉じ→対応する開きは存在しない
+                        {
+                            return false;
+                        }
+
+                        // 最も内側の括弧開きを確認
+                        firstParenthesis = subStringS.Substring(currentWordCount - 1, 1);  
                         if (firstParenthesis != "{")
                         {
                             return false;
                         }
                         else
                         {
-                            if (currentWordCount == 1)
-                            {
-                                subStringS = subStringS.Substring(0, 1) + subStringS.Substring(currentWordCount + 1, subStringS.Length - (currentWordCount + 1));　// 確認後の括弧を削除（左から抽出＋右から抽出を合わせる）
-                            }
-                            else
-                            {
-                                subStringS = subStringS.Substring(0, currentWordCount - 1) + subStringS.Substring(currentWordCount + 1, subStringS.Length - (currentWordCount + 1));　// 確認後の括弧を削除（左から抽出＋右から抽出を合わせる）
-                            }
-                            currentWordCount = 0;  // 初期化
+                            subStringS = subStringS.Remove(currentWordCount - 1, 2); // {}を削除
+                            // 初期化
+                            currentWordCount = 0;
+                            i = -1; // i = 0で再度処理する為
                         }
                         break;
 
@@ -123,7 +127,38 @@ namespace ValidParentheses
                         break;
                 }                     
             }
-            return true;            
+            return true;
+
+            // 最も多い解答例
+            //Stack<char> stack = new Stack<char>();    スタックを使用
+
+            //foreach (char c in s.ToCharArray())   sから一文字ずつ取り出す
+            //{
+            //    if (c == '(' || c == '{' || c == '[') 開きの場合スタックに格納
+            //    {
+            //        stack.Push(c);
+            //    }
+            //    else if (c == ')' && stack.Count != 0 && stack.Peek() == '(') 閉じでかつスタック内のデータ数が0ではない（開きが既にある）かつ中のデータが閉じに対応する開き→スタックから取り出す
+            //    {
+            //        stack.Pop();
+            //    }
+            //    else if (c == '}' && stack.Count != 0 && stack.Peek() == '{')
+            //    {
+            //        stack.Pop();
+            //    }
+            //    else if (c == ']' && stack.Count != 0 && stack.Peek() == '[')
+            //    {
+            //        stack.Pop();
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+
+
+            //}
+
+            //return stack.Count == 0;  全て対応しているなら、スタック内のデータ数は0になっている
         }
     }
 }
